c839b23b0a65757eeea7241a0b8033c63ee79684
Saif Khalifa <63987779+SaifKhalifa@users.noreply.github.com>
2025-02-25 17:27:50 +0200

    Library class added, with management methods to implement later

---
+++ 1_SimpleLibraryManagmentSystem/Library.dart
@@ -0,0 +1,33 @@
+class Library 
+{
+
+  // Add a book to the library
+  void addBook() 
+  {
+    
+  }
+
+  // Retrieve book information
+  String getBookInfo() 
+  {
+  
+  }
+
+  // List all books, with ability to filter by genre
+  void listAllBooks() 
+  {
+    
+  }
+
+  // List books by genre
+  void listBooksByGenre() 
+  {
+    
+  }
+
+  // Remove a book
+  bool removeBook() 
+  {
+  
+  }
+}
